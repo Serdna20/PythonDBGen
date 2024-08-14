ccac3756e0e97a91665a50e9c95a6b66eaea8184
Serdna20 <afgarciasa@ufpso.edu.co>
2024-08-14 17:29:21 -0500

    Added hour.py

---
+++ lines/hour.py
@@ -0,0 +1,16 @@
+import random
+import datetime
+
+def timeSeconds(timeFormat):
+    timeConversion = [3600, 60, 1]
+    seconds = 0
+    for i in range(0, 2):
+        seconds += timeFormat[i] * timeConversion[i]
+    return seconds
+
+def randomTime(limits):
+    minT, maxT = list(map(int, limits[0].split(":"))), list(map(int, limits[1].split(":")))
+    minSec, maxSec = timeSeconds(minT), timeSeconds(maxT)
+    return str(datetime.timedelta(seconds=random.randint(minSec, maxSec)))
+    
+randomTime(["00:20:10", "00:30:45"])
